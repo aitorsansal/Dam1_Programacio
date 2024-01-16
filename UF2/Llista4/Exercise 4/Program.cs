8eeb8e17ce25da859f4cdcfadbacc56a571043d0
aitorsansal <51362433+aitorsansal@users.noreply.github.com>
2024-01-16 18:52:49 +0100

    Update Program.cs

--- UF2/Llista4/Exercise 4/Program.cs
+++ UF2/Llista4/Exercise 4/Program.cs
@@ -26,21 +26,11 @@
         static string WriteRandomWordWithList(int letters)
         {
             Random r = new Random();
-            var toAdd = r.Next(0,2) == 0 ? 65 : 97;
             StringBuilder sb = new();
-            List<char> usableChars = new();
-            for (int i = 65; i < 91; i++)
-            {
-                usableChars.Add((char)i);
-            }
-
-            for (int i = 97; i < 123; i++)
-            {
-                usableChars.Add((char)i);
-            }
             for (int i = 0; i < letters; i++)
             {
-                sb.Append((char)(toAdd + r.Next(0,27)));
+                var toAdd = r.Next(0,2) == 0 ? 65 : 97;
+                sb.Append((char)(toAdd + r.Next(0,26)));
             }
             return sb.ToString();
         }
